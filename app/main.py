b1c82ebff14fb43a6c37e776e959a1f76e6385fd
3bin-05 <ebin05reji@gmail.com>
2026-03-17 22:04:31 +0530

    Initial clean commit

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from app.routes import predict
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="DarkNetra API")
 
+# 🔥 ADD THIS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all (for hackathon)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(predict.router)
 
 @app.get("/")
